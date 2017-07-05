30d077804e71365ca39175e33d8ffb8faacdc9ab
Thommy Jonasson <thommy.jonasson@gmail.com>
2017-07-05 15:47:46 +0200

    minor

--- dev/js/containers/search.js
+++ dev/js/containers/search.js
@@ -18,7 +18,7 @@ class Search extends Component {
         return (
             <div>
                 <h3>Search for item:</h3>
-                <input id="search_field" type="text" onChange={e => this.updateSearch(e.target.value)}/>
+                <input id="search_field" type="text" onChange={e => this.updateSearch(e.target.value.toLowerCase())}/>
                 <br></br>
                 <h3>Result</h3>
                 <div>
@@ -35,10 +35,10 @@ class Search extends Component {
     }
 
     getItems(input){    
-        return this.props.items.filter(function (item){ return item.id.includes(input); });
+        return this.props.items.filter(function (item){ return item.id.toLowerCase().includes(input); });
     }
     getUsersForItem(item){
-        return this.props.clanUsers.filter(u => { return  u.crafting.filter(c => { return c.type == item.type && c.skill > item.skill}); });
+        return this.props.clanUsers.filter(u => { return u.crafting.filter(c => { return c.type == item.type && c.skill > item.skill}); });
     }
 
     updateSearch(input){
@@ -46,17 +46,16 @@ class Search extends Component {
             this.setState({result: []});
             return;            
         }
-        var items = this.getItems(input);
 
-        var results = items.map(i => { return {
+        var results = this.getItems(input).map(i => { return {
                 item: i,
                 users: this.getUsersForItem(i)
             }; 
         });
-
         this.setState({result: results});  
     }
 }
+
 
 // "state.activeUser" is set in reducers/index.js
 function mapStateToProps(state) {
